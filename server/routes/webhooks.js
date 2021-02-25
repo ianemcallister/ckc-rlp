4d1d5aeed3342d4ab63a3f14af78f5e575e4a97e
Ian McAllister <ianmcallister@Ians-Air.home>
2021-02-25 12:46:28 -0800

    Add: Webhook from square triggers enrollment

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -6,6 +6,7 @@ module.exports = (function() {
     'use strict';
     //  DEFINE DEPENDENCIES
     var webhookRoutes = require('express').Router();
+    var ckccrm = require('./crm/ckccrm.js');
 
     //  GET: /test
     webhookRoutes.get('/test', function(req, res) {
@@ -23,7 +24,13 @@ module.exports = (function() {
         //advise of the post body
         console.log(req.body);
 
-        res.sendStatus(200);
+        try {
+            var result = await ckccrm.EnrollReferalCustomer('aoshdgES98hvs', '+15551112222'); 
+            console.log(result);
+            res.sendStatus(200);
+        } catch (error) {
+            console.log(error);
+        } 
 
     });
 
